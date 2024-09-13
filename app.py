db85daa0dc9825225b649a974b52d90f960e19dc
maxime mansuy <mmansuy51@gmail.com>
2024-09-13 21:35:09 +0200

    update

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def upload_image():
         print(f"Exception: {e}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ != '__main__':
+    application = app
